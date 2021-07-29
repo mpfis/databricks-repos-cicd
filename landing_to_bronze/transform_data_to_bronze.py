060a13b64715d94f991e85d0279000993436977e
mpfis <mpfis>
2021-07-29 13:00:38 +0000

    initial commit and test

---
+++ landing_to_bronze/transform_data_to_bronze.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+aget_data = spark.read.csv(dbutils.fs.ls("/databricks-datasets/online_retail/data-001/")[0].path, inferSchema=True, header=True)
+
+# COMMAND ----------
+
+dbutils.widgets.text("country","","")
+
+# COMMAND ----------
+
+country = dbutils.widgets.get("country")
+
+# COMMAND ----------
+
+print(country)
+
+# COMMAND ----------
+
+get_country_data = get_data.filter(f"Country == '{country}'")
+
+# COMMAND ----------
+
+get_country_data.write.saveAsTable("retail_bronze", format="delta", mode="append")
